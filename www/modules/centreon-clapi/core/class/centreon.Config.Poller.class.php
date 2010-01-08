da7edf239a369416c8bc38a970392241e6c2e284
Julien Mathis <jmathis@merethis.com>
2010-01-08 16:07:17 +0000

    git-svn-id: http://svn.modules.centreon.com/centreon-clapi/trunk@4 77bb0176-5a63-4fbe-be93-211753e0ed67

---
+++ www/modules/centreon-clapi/core/class/centreon.Config.Poller.class.php
@@ -0,0 +1,286 @@
+<?php
+
+class CentreonConfigPoller {
+	private $DB;
+	private $resultTest;
+	private $optGen;
+	private $nagiosCFGPath;
+	private $centreon_path;
+	private $centcore_pipe;
+
+	public function CentreonConfigPoller($DB, $centreon_path) {
+		$this->DB = $DB;
+		$this->resultTest = 0;
+		$this->nagiosCFGPath = "$centreon_path/filesGeneration/nagiosCFG/";
+		$this->centreon_path = $centreon_path;
+		$this->resultTest = array("warning" => 0, "errors" => 0);
+		$this->centcore_pipe = "/var/lib/centreon/centcore.cmd";
+	}
+	
+	private function getOptGen() {
+		$DBRESULT =& $this->DB->query("SELECT * FROM options");
+		while ($row =& $DBRESULT->fetchRow()) {
+			$this->optGen[$row["key"]] = $row["value"];
+		}
+		$DBRESULT->free();
+		unset($row);
+	}
+
+	private function testPollerId($id) {
+		$DBRESULT =& $this->DB->query("SELECT id FROM nagios_server WHERE `id` = '$id'");
+		if ($DBRESULT->numRows() != 0)
+			return;
+		else {
+			print "ERROR: Unknown poller...\n";
+			$this->getPollerList($this->format);
+			exit(1);
+		}
+			
+	}
+
+	public function getPollerList($format) {
+		$DBRESULT =& $this->DB->query("SELECT id,name FROM nagios_server ORDER BY id");
+		if ($format == "xml")
+			print "";
+		while ($data =& $DBRESULT->fetchRow()) {
+	    	print $data["id"]."\t".$data["name"]."\n";
+	    }
+		$DBRESULT->free();
+		unset($data);
+	}
+	
+	public function pollerReload($variables) {
+		if (!isset($variables)) {
+			print "Cannot get poller id.";
+			exit(1);
+		}
+		
+		$this->testPollerId($variables);
+		
+		/*
+		 * Get Init Script
+		 */
+		$DBRESULT =& $this->DB->query("SELECT id, init_script FROM nagios_server WHERE localhost = '1' AND ns_activate = '1'");
+		$serveurs =& $DBRESULT->fetchrow();
+		$DBRESULT->free();
+		(isset($serveurs["init_script"])) ? $nagios_init_script = $serveurs["init_script"] : $nagios_init_script = "/etc/init.d/nagios";
+		unset($serveurs);
+	
+		$DBRESULT =& $this->DB->query("SELECT * FROM `nagios_server` WHERE `id` = '$variables'  LIMIT 1");
+		$host = $DBRESULT->fetchRow();
+		$DBRESULT->free();
+			
+		$msg_restart = "";
+		if (isset($host['localhost']) && $host['localhost'] == 1) {
+			$msg_restart = shell_exec("sudo " . $nagios_init_script . " reload");
+		} else { 
+			system("echo 'RELOAD:".$host["id"]."' >> ". $this->centcore_pipe);
+			$msg_restart .= _("OK: A reload signal has been sent to ".$host["name"]);
+		}
+		print $msg_restart;
+		$DBRESULT =& $this->DB->query("UPDATE `nagios_server` SET `last_restart` = '".time()."' WHERE `id` = '".$variables."' LIMIT 1");
+	}
+	
+	public function pollerRestart($variables) {
+		if (!isset($variables)) {
+			print "Cannot get poller id.";
+			exit(1);
+		}
+		
+		$this->testPollerId($variables);
+		
+		/*
+		 * Get Init Script
+		 */
+		$DBRESULT =& $this->DB->query("SELECT id, init_script FROM nagios_server WHERE localhost = '1' AND ns_activate = '1'");
+		$serveurs =& $DBRESULT->fetchrow();
+		$DBRESULT->free();
+		(isset($serveurs["init_script"])) ? $nagios_init_script = $serveurs["init_script"] : $nagios_init_script = "/etc/init.d/nagios";
+		unset($serveurs);
+	
+		$DBRESULT =& $this->DB->query("SELECT * FROM `nagios_server` WHERE `id` = '$variables'  LIMIT 1");
+		$host = $DBRESULT->fetchRow();
+		$DBRESULT->free();	
+		
+		$msg_restart = "";
+		if (isset($host['localhost']) && $host['localhost'] == 1) {
+			$msg_restart = shell_exec("sudo " . $nagios_init_script . " restart");
+		} else { 
+			system("echo 'RESTART:".$variables."' >> ". $this->centcore_pipe);
+			$msg_restart = _("OK: A restart signal has been sent to ".$host["name"]);
+		}
+		print $msg_restart;
+		$DBRESULT =& $this->DB->query("UPDATE `nagios_server` SET `last_restart` = '".time()."' WHERE `id` = '".$variables."' LIMIT 1");
+	}
+	
+	public function pollerTest($format, $variables) {
+		if (!isset($variables)) {
+			print "Cannot get poller id.";
+			exit(1);
+		}
+		
+		$this->testPollerId($variables);
+		/*
+		 * Get Nagios Bin
+		 */
+		$DBRESULT_Servers =& $this->DB->query("SELECT `nagios_bin` FROM `nagios_server` WHERE `ns_activate` = '1' AND `localhost` = '1' LIMIT 1");
+		$nagios_bin = $DBRESULT_Servers->fetchRow();
+		$DBRESULT_Servers->free();
+		
+		$stdout = shell_exec("sudo ".$nagios_bin["nagios_bin"] . " -v ".$this->nagiosCFGPath.$variables."/nagiosCFG.DEBUG");
+		$lines = split("\n", $stdout);
+
+		$msg_debug = "";
+		foreach ($lines as $line) {
+			if (strncmp($line, "Processing object config file", strlen("Processing object config file")) 
+				&& strncmp($line, "Website: http://www.nagios.org", strlen("Website: http://www.nagios.org"))) {
+					$msg_debug .= $line . "\n";
+					/*
+					 * Detect Errors
+					 */
+					if (preg_match("/Total Warnings: ([0-9])*/", $line, $matches))
+						$this->resultTest["warning"] = $matches[1];
+					if (preg_match("/Total Errors: ([0-9])*/", $line, $matches))
+						$this->resultTest["errors"] = $matches[1];
+					if (preg_match("/^Error:/", $line, $matches))
+						$this->resultTest["errors"]++;
+					if (preg_match("/^Errors:/", $line, $matches))
+						$this->resultTest["errors"]++;
+				}
+		}
+		if ($this->resultTest["errors"] != 0) {
+			print "Error: Nagios Poller $variables cannot restart. configuration broker. Please see debug bellow :\n";
+			print "---------------------------------------------------------------------------------------------------";
+			print $msg_debug;
+			print "---------------------------------------------------------------------------------------------------";
+		} else if ($this->resultTest["warning"] != 0) {
+			print "Warning: Nagios Poller $variables can restart but configuration is not optimal. Please see debug bellow :\n";
+			print "---------------------------------------------------------------------------------------------------";
+			print $msg_debug;
+			print "---------------------------------------------------------------------------------------------------";
+		} else {
+			print "OK: Nagios Poller $variables can restart without problem...";
+		}
+		return;
+	}
+	
+	public function pollerGenerate($variables) {
+		require_once "../../../include/configuration/configGenerate/DB-Func.php";
+		require_once "../../../include/common/common-Func.php";
+		
+		$this->testPollerId($variables);
+		
+		$centreon_path = $this->centreon_path;
+		global $pearDB;
+		$pearDB = $this->DB;
+
+		$nagiosCFGPath = $this->nagiosCFGPath;
+		$DebugPath = "filesGeneration/nagiosCFG/";
+		
+		/*
+		 * Init environnement
+		 */
+		if ($this->optGen["version"] == "2.2")
+			require_once $this->centreon_path."/www/class/centreon.class.php";
+		else
+			require_once $this->centreon_path."/www/class/Oreon.class.php";
+
+		require_once $this->centreon_path."/www/class/centreonDB.class.php";
+		require_once $this->centreon_path."/www/class/centreonAuth.class.php";
+		require_once $this->centreon_path."/www/class/centreonLog.class.php";
+		
+		global $oreon;
+		
+		$CentreonLog = new CentreonUserLog(-1, $pearDB);
+		$centreonAuth = new CentreonAuth($this->login, $this->password, 0, $this->DB, $CentreonLog,NULL);
+		$user =& new User($centreonAuth->userInfos, $this->optGen["nagios_version"]);
+	    $oreon = new Oreon($user); 
+		
+		$oreon->user->version = 3; 
+		
+		$tab['id'] = $variables;
+		/*
+		 * Generate dependancies tree.
+		 */
+		global $gbArr;
+		$gbArr = manageDependencies();
+
+		/*
+		 * Generate Configuration
+		 */
+		$path = "../../../include/configuration/configGenerate/";
+		require $path."genCGICFG.php";
+		require $path."genNagiosCFG.php";
+		require $path."genNdomod.php";
+		require $path."genNdo2db.php";
+		require $path."genNagiosCFG-DEBUG.php";
+		require $path."genResourceCFG.php";
+		require $path."genTimeperiods.php";
+		require $path."genCommands.php";
+		require $path."genContacts.php";
+		require $path."genContactGroups.php";
+		require $path."genHosts.php";
+		require $path."genHostTemplates.php";
+		require $path."genHostGroups.php";
+		require $path."genServiceTemplates.php";
+		require $path."genServices.php";
+		require $path."genServiceGroups.php";
+		require $path."genEscalations.php";
+		require $path."genDependencies.php";
+		require $path."centreon_pm.php";
+ 
+ 		print "Configuration files generated for poller ".$variables;
+	}
+	
+	public function cfgMove($variables) {
+		if (!isset($variables)) {
+			print "Cannot get poller id.";
+			exit(1);
+		}
+		/*
+		 * Check poller existance
+		 */
+		$this->testPollerId($variables);
+		
+		/*
+		 * Move files.
+		 */
+		$DBRESULT_Servers =& $this->DB->query("SELECT `cfg_dir` FROM `cfg_nagios` WHERE `nagios_server_id` = '$variables' LIMIT 1");
+		$Nagioscfg = $DBRESULT_Servers->fetchRow();
+		$DBRESULT_Servers->free(); 
+		
+		$DBRESULT_Servers =& $this->DB->query("SELECT * FROM `nagios_server` WHERE `id` = '$variables'  LIMIT 1");
+		$host = $DBRESULT_Servers->fetchRow();
+		$DBRESULT_Servers->free();
+		if (isset($host['localhost']) && $host['localhost'] == 1) {
+			$msg_copy = "";
+			foreach (glob($this->nagiosCFGPath.$variables."/*.cfg") as $filename) {
+				$bool = @copy($filename , $Nagioscfg["cfg_dir"].basename($filename));
+				$filename = array_pop(explode("/", $filename));
+				if (!$bool)
+					$msg_copy .= $this->display_copying_file($filename, " - "._("movement")." KO");
+			}
+			if (strlen($msg_copy) == 0) {
+				$msg_copy .= _("OK: All configuration files copied with success.");
+			}
+		} else {
+			passthru("echo 'SENDCFGFILE:".$host['id']."' >> ".$this->centcore_pipe, $return);
+			if (!isset($msg_copy))
+				$msg_copy = "";
+			$msg_copy .= _("OK: All configuration will be send to '".$host['name']."' by centcore in several minutes.");
+		}	
+		print $msg_copy;
+	}
+	
+	private function display_copying_file($filename = NULL, $status){
+		if (!isset($filename))
+			return ;
+		$str = "- ".$filename." -> ".$status."\n";
+		return $str;
+	}
+	
+	/*
+	 * Method for config file Generation
+	 */
+}
+?>
---
+++ www/modules/centreon-clapi/core/class/centreonAPI.class.php
@@ -0,0 +1,227 @@
+<?php
+/*
+ * Copyright 2005-2009 MERETHIS
+ * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * GPL Licence 2.0.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation ; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
+ * exception to your version of the program, but you are not obliged to do so. If you
+ * do not wish to do so, delete this exception statement from your version.
+ *
+ * For more information : contact@centreon.com
+ *
+ * SVN : $URL$
+ * SVN : $Id$
+ *
+ */
+
+/*
+ * Include Centreon Class
+ */
+require_once "../../../class/centreonDB.class.php";
+require_once "../../../class/centreonXML.class.php";
+
+require_once "./class/centreon.Config.Poller.class.php";
+require_once "./class/centreon.Poller.Management.Class.php";
+
+/*
+ * Declare Centreon API
+ * 
+ */
+class CentreonAPI {
+	public $dateStart;
+	public $login;
+	public $password;
+	public $action; 
+	public $options; 
+	public $args;
+	public $DB;
+	public $DBC;
+	public $DBN;
+	public $format;
+	public $xmlObj;
+	public $debug;
+	public $variables;
+	public $centreon_path;
+
+	public function CentreonAPI($user, $password, $action, $centreon_path, $options) {
+		/*
+		 * Set variables
+		 */
+		$this->debug = 0;
+		$this->login 	= htmlentities($user, ENT_QUOTES);
+		$this->password = htmlentities($password, ENT_QUOTES);
+		$this->action 	= htmlentities($action, ENT_QUOTES);
+		$this->options 	= $options;
+		$this->centreon_path = $centreon_path;
+		if (isset($options["v"]))
+			$this->variables= $options["v"];
+		else
+			$this->variables= "";
+				
+		/*
+  		 * Centreon DB Connexion
+		 */ 
+		$this->DB = new CentreonDB();
+		$this->dateStart = time();
+		if (!isset($this->options["V"]) && !isset($this->options["h"]))
+			$this->checkUser();
+	}
+
+	public function checkUser() {
+		if (!isset($this->login) || $this->login == "") {
+			print "ERROR: Can not connect to centreon without login.\n";
+			$this->printHelp();
+			exit();
+		}
+		if (!isset($this->password) || $this->password == "") {
+			print "ERROR: Can not connect to centreon without password.";
+			$this->printHelp();
+		}
+		
+		/*
+		 * Check Login / Password
+		 */
+		$DBRESULT =& $this->DB->query("SELECT contact_id FROM contact WHERE contact_alias = '".$this->login."' AND contact_passwd = MD5('".$this->password."') AND contact_activate = '1' AND contact_oreon = '1'");
+		if ($DBRESULT->numRows()) {
+			return 1;
+		} else {
+			print "Can not found user '".$this->login."'. User cannot connect\n";
+			exit(1);
+		}
+	}
+
+	public function close() {
+		print "\n";
+	}
+
+	public function setFormat($format) {
+		$this->format = format;
+	}
+
+	public function printHelp() {
+		$this->printLegals();
+		print "This software comes with ABSOLUTELY NO WARRANTY. This is free software,\n";
+		print "and you are welcome to modify and redistribute it under the GPL license\n";
+		print "usage: centreon -u <LOGIN> -p <PASSWORD> -a <ACTION> [-v]\n";
+		print "  -v 	variables \n";
+		print "  -h 	Print help \n";
+		print "  -V 	Print version \n";
+		print "  -a 	Launch action on Centreon\n";
+		print "     Actions are the followings :\n";
+		print "       - POLLERGENERATE: Build nagios configuration for a poller (poller id in -v parameters)\n";
+		print "           #> centreon -u LOGIN -p PASSWORD -a POLLERRESTART -v 1 \n";
+		print "       - POLLERTEST: Test nagios configuration for a poller (poller id in -v parameters)\n";
+		print "           #> centreon -u LOGIN -p PASSWORD -a POLLERTEST -v 1 \n";
+		print "       - CFGMOVE: move nagios configuration for a poller to final directory (poller id in -v parameters)\n";
+		print "           #> centreon -u LOGIN -p PASSWORD -a CFGMOVE -v 1 \n";
+		print "       - POLLERRESTART: Restart a poller (poller id in -v parameters)\n";
+		print "           #> centreon -u LOGIN -p PASSWORD -a POLLERRESTART -v 1 \n";
+		print "       - POLLERRELOAD: Reload a poller poller id in -v parameters)\n";
+		print "           #> centreon -u LOGIN -p PASSWORD -a POLLERRELOAD -v 1 \n";
+		print "\n\n";
+		print "Notes:\n";
+		print "  - Actions can be sen in minuscule chars\n";
+		print "\n";
+	}
+
+	public function getVar($str) {
+		$res = split("=", $str);
+ 		return $res[1];
+	}
+
+	public function initXML() {
+		$this->xmlObj = new CentreonXML();
+	}
+
+	public function launchAction() {
+		$action = strtoupper($this->action);
+ 		if ($this->debug)
+ 			print "DEBUG : $action\n";
+ 		if (method_exists($this, $action))
+ 			$this->$action();
+ 		else
+			print "Sorry unavailable fonction.";
+ 		return;
+	}
+
+	public function printLegals() {
+		$DBRESULT =& $this->DB->query("SELECT * FROM informations WHERE `key` = 'version'");
+ 		$data =& $DBRESULT->fetchRow();
+ 		print "Centreon API version ".$data["value"]." - ";
+ 		print "Copyright Merethis - www.centreon.com\n";
+		unset($data);
+	}
+
+	public function printVersion() {
+		$DBRESULT =& $this->DB->query("SELECT * FROM informations WHERE `key` = 'version'");
+ 		$data =& $DBRESULT->fetchRow();
+ 		print "version ".$data["value"]."\n";
+ 		unset($data);
+	}
+
+	/*
+	 * API POssibilities
+	 */
+
+	public function POLLERLIST() {
+		$poller = new CentreonConfigPoller($this->DB, $this->centreon_path);
+		$poller->getPollerList($this->format);
+	}
+
+	public function POLLERRESTART() {
+		$poller = new CentreonConfigPoller($this->DB, $this->centreon_path);
+		$poller->pollerRestart($this->variables);
+	}
+	
+	public function POLLERRELOAD() {
+		$poller = new CentreonConfigPoller($this->DB, $this->centreon_path);
+		$poller->pollerReload($this->variables);
+	}
+
+	public function POLLERGENERATE() {
+		$poller = new CentreonConfigPoller($this->DB, $this->centreon_path);
+		$poller->pollerGenerate($this->variables);
+	}
+
+	public function POLLERTEST() {
+		$poller = new CentreonConfigPoller($this->DB, $this->centreon_path);
+		$poller->pollerTest($this->format, $this->variables);
+	}
+	
+	public function CFGMOVE() {
+		$poller = new CentreonConfigPoller($this->DB, $this->centreon_path);
+		$poller->cfgMove($this->variables);
+	}
+	
+	public function APPLYCFG() {
+		$poller = new CentreonConfigPoller($this->DB, $this->centreon_path);
+		$poller->pollerGenerate($this->variables);
+		$poller->pollerTest($this->format, $this->variables);
+		$poller->cfgMove($this->variables);
+		$poller->pollerRestart($this->variables);
+	}
+	
+}
+?>
