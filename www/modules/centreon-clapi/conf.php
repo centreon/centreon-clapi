5179ab3e7ecee014c0974952fd7f36aa97972187
Julien Mathis <jmathis@merethis.com>
2010-04-21 09:06:40 +0000

    prepare 1.1
    git-svn-id: http://svn.modules.centreon.com/centreon-clapi/trunk@34 77bb0176-5a63-4fbe-be93-211753e0ed67

--- www/modules/centreon-clapi/conf.php
+++ www/modules/centreon-clapi/conf.php
@@ -1,7 +1,7 @@
 <?php
 $module_conf['centreon-clapi']["rname"] = "Centreon Command Line API";
 $module_conf['centreon-clapi']["name"] = "centreon-clapi";
-$module_conf['centreon-clapi']["mod_release"] = "1.0.0";
+$module_conf['centreon-clapi']["mod_release"] = "1.1.0";
 $module_conf['centreon-clapi']["infos"] = "";
 $module_conf['centreon-clapi']["is_removeable"] = "1";
 $module_conf['centreon-clapi']["author"] = "Merethis Team";
