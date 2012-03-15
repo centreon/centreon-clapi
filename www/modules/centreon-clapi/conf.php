5917319473ab209297fca47f5221fc9cdc096e14
Sylvestre Ho <shotamchay@merethis.com>
2012-03-15 16:55:15 +0000

    prepare package
    git-svn-id: http://svn.modules.centreon.com/centreon-clapi/trunk@315 77bb0176-5a63-4fbe-be93-211753e0ed67

--- www/modules/centreon-clapi/conf.php
+++ www/modules/centreon-clapi/conf.php
@@ -1,7 +1,7 @@
 <?php
 $module_conf['centreon-clapi']["rname"] = "Centreon Command Line API";
 $module_conf['centreon-clapi']["name"] = "centreon-clapi";
-$module_conf['centreon-clapi']["mod_release"] = "1.3.0-b3";
+$module_conf['centreon-clapi']["mod_release"] = "1.3.0";
 $module_conf['centreon-clapi']["infos"] = "A tool for Centreon Management in command line.";
 $module_conf['centreon-clapi']["is_removeable"] = "1";
 $module_conf['centreon-clapi']["author"] = "Merethis Team";
