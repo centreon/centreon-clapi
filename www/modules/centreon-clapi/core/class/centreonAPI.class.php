0ec9f62af0533e90285966ddefd1cdef55037d5d
Sylvestre Ho <sho@merethis.com>
2013-02-04 14:40:35 +0100

    fix #3576; fix url

--- www/modules/centreon-clapi/core/class/centreonAPI.class.php
+++ www/modules/centreon-clapi/core/class/centreonAPI.class.php
@@ -317,7 +317,7 @@ class CentreonAPI {
 		print "           #> ./centreon -u <LOGIN> -p <PASSWORD> -a POLLERLIST\n";
 		print "\n";
 		print "   For more information about configuration objects, please refer to CLAPI wiki:\n";
-		print "      - http://forge.centreon.com/wiki/centreon-clapi/Use \n";
+		print "      - http://documentation.centreon.com/docs/centreon-clapi/ \n";
 		print "\n";
 		print "Notes:\n";
 		print "  - Actions can be written in lowercase chars\n";
