00bff4a0f70d870ce6305be2de7d77aa1f68df30
Sylvestre Ho <sho@merethis.com>
2013-11-21 16:11:06 +0100

    prepare 1.5.3

---
+++ www/modules/centreon-clapi/UPGRADE/centreon-clapi-1.5.3/conf.php
@@ -0,0 +1,10 @@
+<?php
+$upgrade_conf['centreon-clapi']["rname"] = "Centreon Command Line API";
+$upgrade_conf['centreon-clapi']["release_from"] = "1.5.2";
+$upgrade_conf['centreon-clapi']["release_to"] = "1.5.3";
+$upgrade_conf['centreon-clapi']["is_removeable"] = "1";
+$upgrade_conf['centreon-clapi']["author"] = "Merethis";
+$upgrade_conf['centreon-clapi']["lang_files"] = "0";
+$upgrade_conf['centreon-clapi']["sql_files"] = "0";
+$upgrade_conf['centreon-clapi']["php_files"] = "0";
+?>
