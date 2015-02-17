28c398123fc3da60c0d2cb0a4bd349e099ed4df9
rwerquin <rwerquin@merethis.com>
2015-02-17 15:51:50 +0100

    version 1.7.1

--- www/modules/centreon-clapi/conf.php
+++ www/modules/centreon-clapi/conf.php
@@ -1,10 +1,10 @@
 <?php
 $module_conf['centreon-clapi']["rname"] = "Centreon Command Line API";
 $module_conf['centreon-clapi']["name"] = "centreon-clapi";
-$module_conf['centreon-clapi']["mod_release"] = "1.7.0";
+$module_conf['centreon-clapi']["mod_release"] = "1.7.1";
 $module_conf['centreon-clapi']["infos"] = "A tool for Centreon Management in command line.";
 $module_conf['centreon-clapi']["is_removeable"] = "1";
-$module_conf['centreon-clapi']["author"] = "Merethis Team";
+$module_conf['centreon-clapi']["author"] = "Centreon";
 $module_conf['centreon-clapi']["lang_files"] = "0";
 $module_conf['centreon-clapi']["sql_files"] = "0";
 $module_conf['centreon-clapi']["php_files"] = "0";
