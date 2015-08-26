6ba35ce0370e49b7f13d18acf20054615f0b2f27
kevin duret <kduret@centreon.com>
2015-08-26 17:28:35 +0200

    change to version 1.8.0

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ MACRO_FILE_CONF="www/modules/centreon-clapi/etc/api.conf.php"
 
 # Define Centreon Config Books version
 NAME="centreon-clapi"
-VERSION="1.7.1"
+VERSION="1.8.0"
 MODULE=$NAME.$VERSION
 LOG_VERSION="$MODULE installation"
 FILE_CONF="instCentWeb.conf"
